75e009faabfc5479665b19bbaac6be74c00a01fa
Gustavo Marinho <marinho.brandao@unesp.br>
2025-03-24 22:02:35 -0300

    quarto commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,14 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Link } from 'expo-router';
+import {Image} from 'expo-image';
+
+const PlaceholderImage = require ('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
-    <View
-      style={
-        styles.container
-      }
-    >
+    <View style={styles.container}>
+      <View style={styles.imaageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
       <Text style={styles.text}>BEM VINDO A PAGINA DE BASQUETE</Text>
       
       <Text style={styles.text}>O basquetebol, popularmente conhecido como basquete ou básquete,
@@ -16,7 +17,9 @@ export default function Index() {
          bola por dentro de uma cesta e evitar que a bola entre na sua cesta colocada nas extremidades da quadra, 
          seja num ginásio ou ao ar livre. Os jogadores podem caminhar na quadra, desde que driblem a cada passo dado. 
          Também é possível executar um passe, ou seja, passar a bola em direção a um companheiro de equipe.</Text>
+      </View>
     </View>
+
   );
 }
 
@@ -27,6 +30,17 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: 	"#F0B786",
   },
+
+  imaageContainer:{
+    flex: 1,
+  },
+
+  image:{
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+
   text: {
     color: "#000",
     width: '80%',
