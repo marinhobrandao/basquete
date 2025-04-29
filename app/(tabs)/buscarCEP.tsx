22d0ec4cb599e062392651490318b5f7defdef45
Gustavo Marinho <marinho.brandao@unesp.br>
2025-04-28 21:21:25 -0300

    criacao da API

---
+++ app/(tabs)/buscarCEP.tsx
@@ -0,0 +1,7 @@
+import {StatusBar} from 'expo-status-bar';
+import {useState} from 'react';
+import {Button, Keyboard, StyleSheet, Text, TextInput, View, Alert} from 'react-native';
+
+export default function Buscar(){
+    const [cep, setCep] = useState('');
+}
